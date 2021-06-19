abd53fb8e1649d0779cad51947d7d319334e0039
VeljkoZ310 <2018.10.07@online.osmagimnazija.edu.rs>
2021-06-19 13:13:24 +0200

    Uneti koordinate tacaka

--- src/domacijarvis/DomaciJarvis.java
+++ src/domacijarvis/DomaciJarvis.java
@@ -5,22 +5,35 @@
  */
 package domacijarvis;
 
+import java.util.Scanner;
+
 /**
  *
  * @author vzavi
  */
-class Tacka{
-    int x,y;
-    }
+class Tacka {
+
+    int x, y;
+}
+
 public class DomaciJarvis {
 
-    
-    
-    /**
-     * @param args the command line arguments
-     */
+    public static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-        
+        DomaciJarvis dj = new DomaciJarvis();
+        int brTacaka=8;
+        System.out.println("Broj tacaka je 8:");
+        Tacka[] tacke= new Tacka[brTacaka];
+        for (int i = 0; i < tacke.length; i++) {
+            tacke[i]=new Tacka();
+            System.out.print("Unesi x koordinatu za "+i+" tacku:");
+            tacke[i].x=sc.nextInt();
+            System.out.println("");
+            System.out.print("Unesi y koordinatu za "+i+" tacku:");
+            tacke[i].y=sc.nextInt();
+            System.out.println("");
+        }
     }
-    
+
 }
