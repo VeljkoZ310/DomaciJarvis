c674445a23d10a03d2e4e705f290d8c471f461bd
VeljkoZ310 <2018.10.07@online.osmagimnazija.edu.rs>
2021-06-19 13:20:31 +0200

    Kraj

--- src/domacijarvis/DomaciJarvis.java
+++ src/domacijarvis/DomaciJarvis.java
@@ -5,6 +5,7 @@
  */
 package domacijarvis;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -17,16 +18,15 @@ class Tacka {
 }
 
 public class DomaciJarvis {
-    
-    
 
     public static Scanner sc = new Scanner(System.in);
-    
-    private boolean CCW(Tacka p, Tacka q, Tacka r){
+
+    private boolean CCW(Tacka p, Tacka q, Tacka r) {
         int rez = (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y);
-         if (rez >= 0)
-             return false;
-         return true;
+        if (rez >= 0) {
+            return false;
+        }
+        return true;
     }
 
     public void ispisi(Tacka[] tacke, int[] sledeci) {
@@ -38,6 +38,35 @@ public class DomaciJarvis {
         }
     }
 
+    public void Algoritam(Tacka[] tacke) {
+        int n = tacke.length;
+        if (n < 3) {
+            return;
+        }
+        int[] sledeci = new int[n];
+        Arrays.fill(sledeci, -1);
+
+        int levo = 0;
+        for (int i = 1; i < n; i++) {
+            if (tacke[i].x < tacke[levo].x) {
+                levo = i;
+            }
+        }
+        int p = levo, q;
+        do {
+            q = (p + 1) % n;
+            for (int i = 0; i < n; i++) {
+                if (CCW(tacke[p], tacke[i], tacke[q])) {
+                    q = i;
+                }
+            }
+
+            sledeci[p] = q;
+            p = q;
+        } while (p != levo);
+        ispisi(tacke, sledeci);
+    }
+
     public static void main(String[] args) {
         DomaciJarvis dj = new DomaciJarvis();
         int brTacaka = 8;
@@ -45,13 +74,14 @@ public class DomaciJarvis {
         Tacka[] tacke = new Tacka[brTacaka];
         for (int i = 0; i < tacke.length; i++) {
             tacke[i] = new Tacka();
-            System.out.print("Unesi x koordinatu za " + i + " tacku:");
+            System.out.print("Unesi x koordinatu za " + (i + 1) + " tacku:");
             tacke[i].x = sc.nextInt();
             System.out.println("");
-            System.out.print("Unesi y koordinatu za " + i + " tacku:");
+            System.out.print("Unesi y koordinatu za " + (i + 1) + " tacku:");
             tacke[i].y = sc.nextInt();
             System.out.println("");
         }
+        dj.Algoritam(tacke);
     }
 
 }
